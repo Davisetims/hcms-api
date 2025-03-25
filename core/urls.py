897a3b083fc34311cd7c181e5854ecff9748036d
Davisetims <etimsintegration@gmail.com>
2025-03-25 20:57:33 +0300

    `Removed register_user view from core/urls.py and moved create_user_view to a new path 'register/users/'.`

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from core.users import create_user_view,get_users_view, get_user_by_id_view,\
-register_user, authenticate_user
+ authenticate_user
 from core.medical_records import  post_medical_record,post_medical_history , get_medical_records
 from core.prescriptions import post_prescription, get_patient_prescriptions
 from core.appointments import book_appointment, get_appointments ,update_appointment,\
@@ -10,10 +10,9 @@ from core.test_results import post_test_result, get_test_results
  
 
 urlpatterns = [
-    path("users/", create_user_view, name="create-user"),  # Register user (POST)
+    path("register/users/", create_user_view, name="create-user"),  # Register user (POST)
     path("all/users/", get_users_view, name="get-user"),  
     path('users/<str:user_id>/', get_user_by_id_view, name='get-user-by-id'),# Get user by ID (GET)
-    path('register/', register_user, name='register'),
     path('login/', authenticate_user, name='login'),
     path('post/medical-records/', post_medical_record, name='post-medical-record'),
     path('get/user/medical-records/', get_medical_records, name='get-medical-record'),
